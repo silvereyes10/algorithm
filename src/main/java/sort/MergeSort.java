62c26b350560b6834f2d7c70118206925567dc2a
namseh.lee <namseh.lee@nhn.com>
2016-12-20 16:22:55 +0900

    MergeSort 작성

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -1,40 +1,71 @@
 package sort;
 
+import java.lang.reflect.Array;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 import enums.SortOrder;
 
 /**
  * Created by silvereyes10 on 2016-12-15.
  */
 public class MergeSort<T extends Comparable<T>> implements Sort<T> {
-    @Override
-    public T[] sort(T[] sourceArray, SortOrder ord) {
-        return;
-    }
+	@Override
+	public T[] sort(T[] sourceArray, SortOrder ord) {
+		return mergeSort(sourceArray, 0, sourceArray.length, ord);
+	}
 
-    private T[] partition(T[] sourceArray, int startIndex, int endIndex) {
-        if (startIndex >= endIndex) {
-            // sourceArray의 length가 1이면 merge
-        }
-        // sourceArray의 length가 2 이상이면 partition
-        int midIndex = (startIndex + endIndex) / 2;
-        T[] mergeArray1 = partition(sourceArray, startIndex, midIndex);
-        T[] mergetArray2 = partition(sourceArray, midIndex+1, endIndex);
+	private T[] mergeSort(T[] sourceArray, int startIndex, int endIndex, SortOrder ord) {
+		if (sourceArray.length <= 1) {
+			return ArrayUtils.subarray(sourceArray, startIndex, endIndex);
+		}
+		// sourceArray의 length가 2 이상이면 mergeSort
+		int midIndex = (startIndex + endIndex) / 2;
+		T[] sourceArray1 = ArrayUtils.subarray(sourceArray, startIndex, midIndex);
+		T[] sourceArray2 = ArrayUtils.subarray(sourceArray, midIndex, endIndex);
 
-        return null;
-    }
+		sourceArray1 = mergeSort(sourceArray1, 0, sourceArray1.length, ord);
+		sourceArray2 = mergeSort(sourceArray2, 0, sourceArray2.length, ord);
 
-    private T[] merge(T[] sourceArray1, T[] sourceArray2) {
-        return null;
-    }
+		return merge(sourceArray1, sourceArray2, ord);
+	}
 
-    private boolean compare(T target1, T target2, SortOrder ord) {
-        switch (ord) {
-            case ASCENDING:
-                return target1.compareTo(target2) > 0;
-            case DESCENDING:
-                return target1.compareTo(target2) < 0;
-            default:
-                return false;
-        }
-    }
+	private T[] merge(T[] sourceArray1, T[] sourceArray2, SortOrder ord) {
+		int arrayLength = sourceArray1.length + sourceArray2.length;
+
+		T[] resultArray = (T[])Array.newInstance(sourceArray1.getClass().getComponentType(), arrayLength);
+
+		int index = 0;
+		int source1Index = 0;
+		int source2Index = 0;
+
+		while (true) {
+			if (source1Index < sourceArray1.length && source2Index < sourceArray2.length) {
+				if (compare(sourceArray1[source1Index], sourceArray2[source2Index], ord)) {
+					resultArray[index++] = sourceArray1[source1Index++];
+				} else {
+					resultArray[index++] = sourceArray2[source2Index++];
+				}
+			} else if (source1Index < sourceArray1.length) {
+				resultArray[index++] = sourceArray1[source1Index++];
+			} else if (source2Index < sourceArray2.length) {
+				resultArray[index++] = sourceArray2[source2Index++];
+			} else {
+				break;
+			}
+		}
+
+		return resultArray;
+	}
+
+	private boolean compare(T target1, T target2, SortOrder ord) {
+		switch (ord) {
+			case ASCENDING:
+				return target1.compareTo(target2) < 0;
+			case DESCENDING:
+				return target1.compareTo(target2) > 0;
+			default:
+				return false;
+		}
+	}
 }
---
+++ src/test/java/sort/MergeSortTest.java
@@ -0,0 +1,43 @@
+/*
+ * @(#)BubbleSortTest.java  2016.12.11
+ *
+ * Copyright 2016 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package sort;
+
+import org.junit.Test;
+
+import enums.SortOrder;
+import sort.utils.ArrayGenerator;
+
+/**
+ *
+ * @author NAVER
+ */
+public class MergeSortTest {
+	private MergeSort<Integer> mergeSort = new MergeSort<>();
+
+	@Test
+	public void ascSort() throws Exception {
+		Integer[] intArray = ArrayGenerator.getRandomIntegerArray();
+		System.out.println("Ascending Init Print Array : ");
+		ArrayGenerator.printArray(intArray);
+
+		System.out.println("Ascending Result Print Array : ");
+		Integer[] resultArray = mergeSort.sort(intArray, SortOrder.ASCENDING);
+		ArrayGenerator.printArray(resultArray);
+	}
+
+	@Test
+	public void descSort() throws Exception {
+		Integer[] intArray = ArrayGenerator.getRandomIntegerArray();
+		System.out.println("Decending Init Print Array : ");
+		ArrayGenerator.printArray(intArray);
+
+		System.out.println("Decending Result Print Array : ");
+		Integer[] resultArray = mergeSort.sort(intArray, SortOrder.DESCENDING);
+		ArrayGenerator.printArray(resultArray);
+	}
+
+}
