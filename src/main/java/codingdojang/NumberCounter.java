eccdc8ad9955de9d0fff0bf0e75a0cd7724fcfd3
silvereyes10 <silvereyes10@gmail.com>
2017-01-10 19:42:23 +0900

    NumberCounter 수정#2

--- src/main/java/codingdojang/NumberCounter.java
+++ src/main/java/codingdojang/NumberCounter.java
@@ -4,13 +4,12 @@ package codingdojang;
  * Created by NAVER on 2017-01-04.
  */
 public class NumberCounter {
-
 	public int getCount(int endNumber) {
-		int count = 0;
 		if (endNumber <= 10) {
 			return 1;
 		}
 
+		int count = 0;
 		int dividedCount = endNumber / 10;
 
 		count += getCount(dividedCount) * 10 + dividedCount;
